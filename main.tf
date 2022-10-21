7bd5476b92127b58f2beb8b928e7450ed01e1c75
praveenarupi <praveenarupi@gmail.com>
2022-10-21 19:19:38 +0530

    updating ./tf-module-rabbitmq

--- main.tf
+++ main.tf
@@ -18,6 +18,12 @@ resource "aws_security_group" "main" {
     protocol    = "tcp"
     cidr_blocks = [var.vpc_cidr_block, var.WORKSTATION_IP]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   tags = {
     Name = "${var.env}-rabbitmq"
   }
