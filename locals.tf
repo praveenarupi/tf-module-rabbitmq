6d35b380c3670a36165508c3517bd5cb79917f51
praveenarupi <praveenarupi@gmail.com>
2022-10-21 18:41:38 +0530

    updating ./tf-module-rabbitmq

--- locals.tf
+++ locals.tf
@@ -3,4 +3,4 @@ locals {
   password = element(split("/", data.aws_ssm_parameter.credentials.value), 1)
   ssh_username = element(split("/", data.aws_ssm_parameter.ssh_credentials.value), 0)
   ssh_password = element(split("/", data.aws_ssm_parameter.ssh_credentials.value), 1)
-}x
+}
